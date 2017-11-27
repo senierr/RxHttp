ccdf1b650b6fa505362ffba09e5d9a8cfd951c01
senierr <senierr_zhou@163.com>
2017-11-27 15:31:28 +0800

    移除断点下载测试

--- app/src/main/java/com/senierr/simple/MainActivity.java
+++ app/src/main/java/com/senierr/simple/MainActivity.java
@@ -3,16 +3,13 @@ package com.senierr.simple;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.senierr.sehttp.SeHttp;
 import com.senierr.sehttp.callback.FileCallback;
-import com.senierr.sehttp.callback.FileCallback1;
 import com.senierr.sehttp.callback.JsonCallback;
 
 import java.io.File;
@@ -27,26 +24,6 @@ public class MainActivity extends AppCompatActivity {
     public static final String URL = "https://www.baidu.com";
     public static final String URL_DOWNLOAD = "http://dldir1.qq.com/weixin/Windows/WeChatSetup.exe";
 
-    @BindView(R.id.btn_request)
-    Button btnRequest;
-    @BindView(R.id.btn_pause1)
-    Button btnPause1;
-    @BindView(R.id.btn_download1)
-    Button btnDownload1;
-    @BindView(R.id.pb_progress1)
-    ProgressBar pbProgress1;
-    @BindView(R.id.btn_pause2)
-    Button btnPause2;
-    @BindView(R.id.btn_download2)
-    Button btnDownload2;
-    @BindView(R.id.pb_progress2)
-    ProgressBar pbProgress2;
-    @BindView(R.id.btn_pause3)
-    Button btnPause3;
-    @BindView(R.id.btn_download3)
-    Button btnDownload3;
-    @BindView(R.id.pb_progress3)
-    ProgressBar pbProgress3;
     @BindView(R.id.tv_text)
     TextView tvText;
 
@@ -65,26 +42,14 @@ public class MainActivity extends AppCompatActivity {
         SeHttp.getInstance().cancelTag(this);
     }
 
-    @OnClick({R.id.btn_request, R.id.btn_pause1, R.id.btn_download1, R.id.btn_pause2, R.id.btn_download2, R.id.btn_pause3, R.id.btn_download3})
+    @OnClick({R.id.btn_request, R.id.btn_download})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_request:
                 request();
                 break;
-            case R.id.btn_pause1:
-                SeHttp.getInstance().cancelAll();
-                startPoint = _currentSize;
-                break;
-            case R.id.btn_download1:
+            case R.id.btn_download:
                 download();
-                break;
-            case R.id.btn_pause2:
-                break;
-            case R.id.btn_download2:
-                break;
-            case R.id.btn_pause3:
-                break;
-            case R.id.btn_download3:
                 break;
         }
     }
@@ -135,18 +100,13 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    long _totalSize;
-    long _currentSize;
-    long startPoint;
-
     /**
      * 下载
      */
     private void download() {
         SeHttp.get(URL_DOWNLOAD)
                 .tag(this)
-                .addHeader("RANGE", "bytes=" + startPoint + "-")
-                .execute(new FileCallback1(new File(fileDir), "WeChatSetup.exe", startPoint) {
+                .execute(new FileCallback(new File(fileDir), "WeChatSetup.exe") {
                     @Override
                     public boolean onDiff(Response response, File destFile) {
                         // 判断destFile是否是需要下载的文件，默认返回false
@@ -155,20 +115,9 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onDownloadProgress(long totalSize, long currentSize, int progress) {
-                        tvText.setText("totalSize: " + totalSize + "\n currentSize: " + currentSize + "\n onProgress: " + progress);
-                        if (_currentSize == 0) {
-                            _totalSize = totalSize;
-                            pbProgress1.setMax((int) _totalSize);
-                        }
-
-                        if (_totalSize == totalSize) {
-                            _currentSize = currentSize;
-                        } else {
-                            _currentSize = startPoint + currentSize;
-                        }
-
-                        pbProgress1.setProgress((int) _currentSize);
-                        Log.e("download", _totalSize + ": " + startPoint + ", " + _currentSize);
+                        tvText.setText("totalSize: " + totalSize + "\n" +
+                                "currentSize: " + currentSize + "\n" +
+                                "onProgress: " + progress + "%");
                     }
 
                     @Override
